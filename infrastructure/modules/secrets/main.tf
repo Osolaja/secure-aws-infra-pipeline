cdb9a3e3d3191c0eb925821ebfc5ef9a0fbb57d8
Ayomide Osolaja <ayomideosolaja1@gmail.com>
2026-04-03 11:51:23 -0500

    new run

--- infrastructure/modules/secrets/main.tf
+++ infrastructure/modules/secrets/main.tf
@@ -1,10 +1,15 @@
+resource "random_id" "suffix" {
+  byte_length = 2
+}
+
+
 resource "random_password" "db" {
   length  = 16
   special = true
 }
 
 resource "aws_secretsmanager_secret" "db" {
-  name = "${var.environment}-rds-credentials"
+  name = ""${var.environment}-rds-credentials-${random_id.suffix.hex}"
 
   tags = {
     Name        = "${var.environment}-rds-credentials"
