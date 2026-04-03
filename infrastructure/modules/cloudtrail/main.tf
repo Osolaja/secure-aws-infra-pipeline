a66ea40b882a4c6418310eac2b1e1e658624910e
Ayomide Osolaja <ayomideosolaja1@gmail.com>
2026-04-03 12:23:34 -0500

    new run

--- infrastructure/modules/cloudtrail/main.tf
+++ infrastructure/modules/cloudtrail/main.tf
@@ -4,7 +4,7 @@ resource "random_id" "suffix" {
 
 resource "aws_s3_bucket" "cloudtrail_logs" {
   bucket = "${var.environment}-cloudtrail-logs-${random_id.suffix.hex}"
-
+  force_destroy = true
   tags = {
     Name        = "${var.environment}-cloudtrail-logs"
     Environment = var.environment
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_public_access_block" "cloudtrail_logs" {
 
 resource "aws_s3_bucket_policy" "cloudtrail_logs" {
   bucket = aws_s3_bucket.cloudtrail_logs.id
-  force_destroy = true
+  
 
   policy = jsonencode({
     Version = "2012-10-17"
